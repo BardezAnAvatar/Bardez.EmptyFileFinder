2aaafb50bd28038497165f86c30f29e18590f2a9
Erik Frantz <bardez@bardez.net>
2025-04-18 11:13:26 -0500

    Add in Appsettings for the path

---
+++ Bardez.EmptyFileFinder/Configuration/CheckerOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Bardez.EmptyFileFinder.Configuration
+{
+    public class CheckerOptions : IOptions<CheckerOptions>
+    {
+        public static readonly string ConfigurationSection = nameof(CheckerOptions);
+
+        public string Path { get; set; } = string.Empty;
+
+        public CheckerOptions Value => this;
+    }
+}
